33aa3cc8669fb7de84f67605142e0df8404b9d4d
Anton Kesy <anton@kesy.de>
2025-05-03 23:46:52 +0200

    add data mount

---
+++ scripts/mount-nas.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install -y cifs-utils
+
+sudo mkdir -p /mnt/music
+sudo mkdir -p /mnt/movies
+sudo mkdir -p /mnt/books
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cp "$SCRIPT_DIR"/mount/smb-credentials /etc/smb-credentials
+sudo chmod 600 /etc/smb-credentials
+
+cp "$SCRIPT_DIR"/mount/fstab /etc/fstab
