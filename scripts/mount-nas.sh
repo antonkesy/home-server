c25a9926855e14e62cc60084d3c5d35331a9fe11
Anton Kesy <anton@kesy.de>
2025-05-03 23:51:58 +0200

    test mounts

--- scripts/mount-nas.sh
+++ scripts/mount-nas.sh
@@ -13,3 +13,10 @@ sudo cp "$SCRIPT_DIR"/mount/smb-credentials /etc/smb-credentials
 sudo chmod 600 /etc/smb-credentials
 
 sudo tee -a /etc/fstab < "$SCRIPT_DIR"/mount/fstab
+
+# test mount
+sudo systemctl daemon-reload
+sudo mount -a
+ls /mnt/music
+ls /mnt/movies
+ls /mnt/books
