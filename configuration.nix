3f757f5f960bb764383c73f3af71e4df524c03cc
Anton Kesy <anton@kesy.de>
2026-01-15 16:18:58 +0100

    add r8125 driver

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,41 @@
 { config, pkgs, ... }:
 
+let
+  # Realtek r8125 2.5G Ethernet driver
+  r8125 = config.boot.kernelPackages.callPackage ({ stdenv, lib, kernel, fetchFromGitHub }:
+    stdenv.mkDerivation rec {
+      pname = "r8125";
+      version = "9.015.00";
+
+      src = fetchFromGitHub {
+        owner = "antonkesy";
+        repo = "r8125";
+        rev = "80c1b25847c996d3b92a92d1ac6856b0c5dff4b6";
+        hash = "sha256-5P5FLT9rV6MdHlp9xKj0Zyb1Q2fajZu7h9YPRdxnWOw=";
+      };
+
+      sourceRoot = "source/src";
+      hardeningDisable = [ "pic" "format" ];
+      nativeBuildInputs = kernel.moduleBuildDependencies;
+
+      makeFlags = [
+        "KERNELDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+      ];
+
+      installPhase = ''
+        mkdir -p $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/ethernet/realtek
+        cp r8125.ko $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/ethernet/realtek/
+      '';
+
+      meta = with lib; {
+        description = "Realtek RTL8125 2.5 Gigabit Ethernet driver";
+        homepage = "https://github.com/antonkesy/r8125";
+        license = licenses.gpl2Only;
+        platforms = platforms.linux;
+        maintainers = [];
+      };
+    }) {};
+in
 {
   imports = [
     ./hardware-configuration.nix
@@ -10,6 +46,10 @@
 
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Realtek r8125 network driver
+  boot.extraModulePackages = [ r8125 ];
+  boot.kernelModules = [ "r8125" ];
 
   # Networking
   networking.hostName = "home-server";
