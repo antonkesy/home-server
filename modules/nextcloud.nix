a95b9a42fd5c7b85b31663e588210a4ff7d950aa
Anton Kesy <anton@kesy.de>
2026-01-27 15:53:49 +0100

    upgrade nextcloud

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -3,7 +3,7 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud31;
+    package = pkgs.nextcloud32;
     hostName = "localhost";
     config = {
       adminpassFile = "/var/lib/nextcloud/admin-pass";
