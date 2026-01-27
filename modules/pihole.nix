b26863b37ee7a5b20f201e8c594f8108a1cf8660
Anton Kesy <anton@kesy.de>
2026-01-27 17:47:38 +0100

    add more block lists

--- modules/pihole.nix
+++ modules/pihole.nix
@@ -18,6 +18,56 @@
         enabled = true;
         description = "StevenBlack hosts";
       }
+      {
+        url = "https://adaway.org/hosts.txt";
+        enabled = true;
+        description = "AdAway hosts";
+      }
+      {
+        url = "https://someonewhocares.org/hosts/zero/hosts";
+        enabled = true;
+        description = "Someone Who Cares hosts";
+      }
+      {
+        url = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext";
+        enabled = true;
+        description = "Peter Lowe's Ad and tracking server list";
+      }
+      {
+        url = "https://raw.githubusercontent.com/blocklistproject/Lists/master/ads.txt";
+        enabled = true;
+        description = "Block List Project - Ads";
+      }
+      {
+        url = "https://raw.githubusercontent.com/blocklistproject/Lists/master/tracking.txt";
+        enabled = true;
+        description = "Block List Project - Tracking";
+      }
+      {
+        url = "https://raw.githubusercontent.com/blocklistproject/Lists/master/malware.txt";
+        enabled = true;
+        description = "Block List Project - Malware";
+      }
+      {
+        url = "https://raw.githubusercontent.com/blocklistproject/Lists/master/phishing.txt";
+        enabled = true;
+        description = "Block List Project - Phishing";
+      }
+      {
+        url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/multi.txt";
+        enabled = true;
+        description = "HaGeZi Multi PRO - Comprehensive ad/tracking/malware blocklist";
+      }
+      {
+        url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.txt";
+        enabled = true;
+        description = "HaGeZi Threat Intelligence Feeds - Malware, phishing, scam";
+      }
+      {
+        url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/popupads.txt";
+        enabled = true;
+        description = "HaGeZi Pop-Up Ads - Blocks annoying pop-up ads";
+      }
     ];
     openFirewallDNS = true;
   };
