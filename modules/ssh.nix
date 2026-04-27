fb98e8a9552cbc66cecf824a032e4b1ecb7105c9
Anton Kesy <anton@kesy.de>
2026-04-27 17:35:25 +0200

    add ssh-agent

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,6 +1,15 @@
 { config, pkgs, ... }:
 
 {
+  # Run a user ssh-agent managed by NixOS, so SSH_AUTH_SOCK is set automatically.
+  programs.ssh = {
+    startAgent = true;
+    extraConfig = ''
+      Host *
+        AddKeysToAgent yes
+    '';
+  };
+
   services.openssh = {
     enable = true;
     settings = {
